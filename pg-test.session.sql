7781878c2a06bef2ee6ebb796d5a84052ccd884b
freshcodeAlice <freshjs.ofte@gmail.com>
2024-03-01 11:54:47 +0200

    postgres import/export

--- pg-test.session.sql
+++ pg-test.session.sql
@@ -1,1 +1,18 @@
-DELETE FROM rents;
+CREATE TABLE cats (
+    name varchar(200),
+    age int,
+    color varchar(200),
+    favouriteFood text
+);
+
+/*
+Імпорт з файлу
+\COPY cats FROM '/tmp/cats.csv' DELIMITER ',' CSV HEADER;
+
+Використання папки /tmp дозволяє серверу читати вміст файла для імпорта
+
+Еспорт до файлу
+\COPY movies TO '/tmp/movies.csv' DELIMITER ',' CSV HEADER;
+
+
+*/
